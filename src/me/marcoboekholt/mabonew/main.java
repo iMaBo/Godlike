3a0e5724d3a90c4c429693918826545a18221bfe
marcoboekholt <dhrmabo@gmail.com>
2013-05-12 01:42:00 +0200

    Changed Tools description and removed needless spaces

--- src/me/marcoboekholt/mabonew/main.java
+++ src/me/marcoboekholt/mabonew/main.java
@@ -26,63 +26,63 @@ public class main extends JavaPlugin {
                 Player player = (Player)sender;
                 if(cmd.getName().equalsIgnoreCase("Sword")){
                 	ItemStack sword = new ItemStack(Material.DIAMOND_SWORD, 1);            
-                		sword.addEnchantment(Enchantment.KNOCKBACK, 2);
-                		sword.addEnchantment(Enchantment.DAMAGE_ALL, 5);
-                		sword.addEnchantment(Enchantment.FIRE_ASPECT, 2);
-                		PlayerInventory pl = player.getInventory();
-                		pl.addItem(sword);
-                    	player.sendMessage(ChatColor.DARK_RED + ("*~*") + ChatColor.GREEN + (" You got a sword!"));
+                	sword.addEnchantment(Enchantment.KNOCKBACK, 2);
+                	sword.addEnchantment(Enchantment.DAMAGE_ALL, 5);
+                	sword.addEnchantment(Enchantment.FIRE_ASPECT, 2);
+                	PlayerInventory pl = player.getInventory();
+                	pl.addItem(sword);
+                    player.sendMessage(ChatColor.DARK_RED + ("*~*") + ChatColor.GREEN + (" You got a sword!"));
                 }
                 else if(cmd.getName().equalsIgnoreCase("Bow")){
-                        ItemStack bow = new ItemStack(Material.BOW, 1);
-                        ItemStack arrow = new ItemStack(Material.ARROW, 64);
-                        bow.addEnchantment(Enchantment.ARROW_DAMAGE, 5);
-                        bow.addEnchantment(Enchantment.ARROW_INFINITE, 1);
-                        bow.addEnchantment(Enchantment.ARROW_FIRE, 1);
-                        bow.addEnchantment(Enchantment.ARROW_KNOCKBACK, 2);
-                        PlayerInventory pl = player.getInventory();
-                        pl.addItem(bow);
-                        pl.addItem(arrow);
-                        player.sendMessage(ChatColor.DARK_RED + ("*~*") + ChatColor.GREEN + (" You got a bow and sword!"));
+                    ItemStack bow = new ItemStack(Material.BOW, 1);
+                    ItemStack arrow = new ItemStack(Material.ARROW, 64);
+                    bow.addEnchantment(Enchantment.ARROW_DAMAGE, 5);
+                    bow.addEnchantment(Enchantment.ARROW_INFINITE, 1);
+                    bow.addEnchantment(Enchantment.ARROW_FIRE, 1);
+                    bow.addEnchantment(Enchantment.ARROW_KNOCKBACK, 2);
+                    PlayerInventory pl = player.getInventory();
+                    pl.addItem(bow);
+                    pl.addItem(arrow);
+                    player.sendMessage(ChatColor.DARK_RED + ("*~*") + ChatColor.GREEN + (" You got a bow and sword!"));
                 }
                 else if(cmd.getName().equalsIgnoreCase("Armour")){
-                        ItemStack helmet = new ItemStack(Material.DIAMOND_HELMET, 1);
-                        helmet.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
-                        helmet.addEnchantment(Enchantment.PROTECTION_FIRE, 4);
-                        helmet.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 4);
-                        helmet.addEnchantment(Enchantment.PROTECTION_PROJECTILE, 4);
-                        helmet.addEnchantment(Enchantment.THORNS, 3);
-                        helmet.addEnchantment(Enchantment.OXYGEN, 3);
-                        helmet.addEnchantment(Enchantment.WATER_WORKER, 1);
+                    ItemStack helmet = new ItemStack(Material.DIAMOND_HELMET, 1);
+                    helmet.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+                    helmet.addEnchantment(Enchantment.PROTECTION_FIRE, 4);
+                    helmet.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 4);
+                    helmet.addEnchantment(Enchantment.PROTECTION_PROJECTILE, 4);
+                    helmet.addEnchantment(Enchantment.THORNS, 3);
+                    helmet.addEnchantment(Enchantment.OXYGEN, 3);
+                    helmet.addEnchantment(Enchantment.WATER_WORKER, 1);
                        
-                        ItemStack body = new ItemStack(Material.DIAMOND_CHESTPLATE, 1);
-                        body.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
-                        body.addEnchantment(Enchantment.PROTECTION_FIRE, 4);
-                        body.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 4);
-                        body.addEnchantment(Enchantment.PROTECTION_PROJECTILE, 4);
-                        body.addEnchantment(Enchantment.THORNS, 3);
+                    ItemStack body = new ItemStack(Material.DIAMOND_CHESTPLATE, 1);
+                    body.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+                    body.addEnchantment(Enchantment.PROTECTION_FIRE, 4);
+                    body.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 4);
+                    body.addEnchantment(Enchantment.PROTECTION_PROJECTILE, 4);
+                    body.addEnchantment(Enchantment.THORNS, 3);
+                      
+                    ItemStack legs = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
+                    legs.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+                    legs.addEnchantment(Enchantment.PROTECTION_FIRE, 4);
+                    legs.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 4);
+                    legs.addEnchantment(Enchantment.PROTECTION_PROJECTILE, 4);
+                    legs.addEnchantment(Enchantment.THORNS, 3);
                        
-                        ItemStack legs = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
-                        legs.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
-                        legs.addEnchantment(Enchantment.PROTECTION_FIRE, 4);
-                        legs.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 4);
-                        legs.addEnchantment(Enchantment.PROTECTION_PROJECTILE, 4);
-                        legs.addEnchantment(Enchantment.THORNS, 3);
-                       
-                        ItemStack boots = new ItemStack(Material.DIAMOND_BOOTS, 1);
-                        boots.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
-                        boots.addEnchantment(Enchantment.PROTECTION_FIRE, 4);
-                        boots.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 4);
-                        boots.addEnchantment(Enchantment.PROTECTION_PROJECTILE, 4);
-                        boots.addEnchantment(Enchantment.THORNS, 3);
-                        boots.addEnchantment(Enchantment.PROTECTION_FALL, 4);
+                    ItemStack boots = new ItemStack(Material.DIAMOND_BOOTS, 1);
+                    boots.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+                    boots.addEnchantment(Enchantment.PROTECTION_FIRE, 4);
+                    boots.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 4);
+                    boots.addEnchantment(Enchantment.PROTECTION_PROJECTILE, 4);
+                    boots.addEnchantment(Enchantment.THORNS, 3);
+                    boots.addEnchantment(Enchantment.PROTECTION_FALL, 4);
  
-                        PlayerInventory pl = player.getInventory();
-                        pl.addItem(helmet);
-                        pl.addItem(body);
-                        pl.addItem(legs);
-                        pl.addItem(boots);
-                        player.sendMessage(ChatColor.DARK_RED + ("*~*") + ChatColor.GREEN + (" You got the armour!"));
+                    PlayerInventory pl = player.getInventory();
+                    pl.addItem(helmet);
+                    pl.addItem(body);
+                    pl.addItem(legs);
+                    pl.addItem(boots);
+                    player.sendMessage(ChatColor.DARK_RED + ("*~*") + ChatColor.GREEN + (" You got the armour!"));
                 }
                 else if(cmd.getName().equalsIgnoreCase("Tools")){
                 	ItemStack pickaxe = new ItemStack(Material.DIAMOND_PICKAXE, 1);
